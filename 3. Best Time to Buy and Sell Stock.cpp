81c7680adf2ed5fbd3c6b7d894a5039ec6a9aa30
Venkatesh Thirunagiri <tvenkatesh799@gmail.com>
2023-06-30 20:57:27 -0500

    Create Best Time to Buy and Sell Stock.cpp

---
+++ 3. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,43 @@
+// Problem link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+/*
+  You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+  You want to maximize your profit by choosing a single day to buy one stock and 
+  
+  choosing a different day in the future to sell that stock.
+
+  Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+  Example 1:
+  
+  Input: prices = [7,1,5,3,6,4]
+  Output: 5
+  Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+  Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+  */
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // intially my profit will be 0 and minimum element will be day1 price
+        int profit = 0;
+        int mini = prices[0];
+        int cost = 0;
+        for(int i = 0; i < prices.size(); i++) {
+            // current cost will be preseent day price and minimum price before current day
+            cost = prices[i] - mini;
+          
+            profit = max(profit, cost);
+          
+            //updating minimum value
+            mini = min(mini, prices[i]);
+        }
+        return profit;
+
+    }
+};
