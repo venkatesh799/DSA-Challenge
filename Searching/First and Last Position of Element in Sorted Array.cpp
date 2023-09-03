36f0f144a52df12bfcac40040aa70d1587e06b3f
Venkatesh Thirunagiri <tvenkatesh799@gmail.com>
2023-09-02 21:30:41 -0500

    Update First and Last Position of Element in Sorted Array.cpp

--- Searching/First and Last Position of Element in Sorted Array.cpp
+++ Searching/First and Last Position of Element in Sorted Array.cpp
@@ -49,3 +49,38 @@ public:
         return result;
     }
 };
+// Approach 2 - used one function for both first and last position
+class Solution {
+public:
+    int first_index(vector<int> A, int key, int start, int end, bool first_pos) {
+        int res = -1;
+        int mid;
+        while(start <= end) {
+            mid = (start) + (end - start)/2;
+            if(A[mid] == key) {
+                res = mid;
+                if(first_pos)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            else if(key > A[mid]) {
+                start = mid + 1;
+            }
+            else if(key < A[mid]) {
+                end = mid -1;
+            }
+
+        }
+        return res;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> result(2, -1);
+        result[0] = first_index(nums, target, 0, nums.size()-1, true);
+        if(result[0] == -1) {
+            return result;
+        }
+        result[1] = first_index(nums, target, 0, nums.size()-1,false);
+        return result;
+    }
+};
