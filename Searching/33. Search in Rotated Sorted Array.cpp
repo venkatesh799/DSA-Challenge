2a6c55a4cdd79534e39329e9971bf92d9b7824d2
Venkatesh Thirunagiri <tvenkatesh799@gmail.com>
2023-09-04 22:00:24 -0500

    Create 33. Search in Rotated Sorted Array.cpp

---
+++ Searching/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,36 @@
+// 33. Search in Rotated Sorted Array
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int mid;
+       int low = 0;
+       int high = nums.size() - 1;
+       int prev;
+       int next;
+       while(low <= high) {
+           mid = (low + high)/2;
+           if(nums[mid] == target) {
+               return mid;
+           }
+           else if(nums[mid] <= nums[high]) {
+               if(target > nums[mid] && target <= nums[high]) {
+                   low = mid + 1;
+               }
+               else {
+                   high = mid - 1;
+               }
+           }
+           else if(nums[mid] >= nums[low]) {
+               if(target >= nums[low] && target < nums[mid]) {
+                   high = mid - 1;
+               }
+               else {
+                   low = mid + 1;
+               }
+           }
+       }
+       return -1;
+    }
+};
